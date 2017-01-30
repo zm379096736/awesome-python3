b91ebd32f949a9dd9539d5d991a7f5aeca37afbe
mingzeng96 <mingzeng96@gmail.com>
2017-01-31 02:46:37 +0800

    day 8

---
+++ www/config_default.py
@@ -0,0 +1,14 @@
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
